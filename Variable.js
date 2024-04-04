33bdc7ffd9696b924196ba9a73bcd6d3891d8430
Atul chaurasiya <atulchaurasiysa66602@gmail.com>
2024-04-04 23:58:28 +0530

    commit

---
+++ Variable.js
@@ -0,0 +1,13 @@
+//  variable 
+
+// variables are like container for storing the data
+// there is three keywords to declare the variable
+// let ,var and const
+
+let a=10;
+const str="Atul chaurasiya";
+var b=1000;
+
+console.log(a);
+console.log(str);
+console.log(b);
