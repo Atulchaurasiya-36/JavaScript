8c8d57d8348a3b1e9f831b2b289f11a80cb98714
Atul chaurasiya <atulchaurasiysa66602@gmail.com>
2024-04-06 00:08:03 +0530

    commit

---
+++ datatype.js
@@ -0,0 +1,61 @@
+//  Data types 
+// there is two types of data type -
+// 1. primitive datatype or predefine 
+// 2. non-primitive datatype
+
+//  primitive data types:
+// a. Number
+// b. BigInt
+// c.boolean
+// d. string
+// e.undefined
+// f.null
+// symbol
+
+// example
+
+// Numbers:
+let length = 16;
+let weight = 7.5;
+console.log(length);
+console.log(weight);
+console.log(typeof length)  // number
+console.log( typeof weight); // number
+
+// Strings:
+let firstName = "Atul";
+let lastName = "chaurasiya";
+console.log(firstName,lastName) ; // Atul chaurasiya
+console.log( typeof firstName, typeof lastName); // string string
+
+// Booleans
+let x = true;
+let y = false;
+ console.log(x , y); //
+ console.log(typeof x);
+
+//  bigint
+let z = 9999999999999999n;
+console.log(z);
+console.log( typeof z);
+
+// undefined
+let car;
+console.log(car) ; // udefined
+
+console.log( typeof car) // undefined
+
+// object
+
+const student={
+    name:"Atul",
+    rollNo:36,
+    gmail:"abc@gmail.com"
+}
+console.log(student.name);  // Atul
+console.log(typeof student); // object
+ 
+
+
+
+
