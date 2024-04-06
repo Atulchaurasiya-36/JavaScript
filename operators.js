5da3d1c37c15eb6d8a74d62f20c4379981366b3e
Atul chaurasiya <atulchaurasiysa66602@gmail.com>
2024-04-06 21:44:11 +0530

    commit

---
+++ operators.js
@@ -0,0 +1,57 @@
+//  operators are symbol which is used to perform different type of mathematical and logical computation
+
+// for example
+
+let a=10 ;
+let b=20;
+console.log(a+b); // 30 : '+' is operator which is used to perform addition 
+
+// types of operator
+
+// 1 Arithmetic Operators '+', '-' , '*' , '/' , '**' , '%' , '++' ,'--'
+
+ console.log(a+b) //  30
+ console.log(b-a) //10
+ console.log(a*b) // 200
+ console.log(b/a) //2
+ console.log(a**2) // 100
+ console.log(b%a) //0
+ console.log(++a) //11
+ console.log(--b) //19
+
+//  2 Assignment Operators: assignment operators used to assign value to variable- '=' , '+=' , '-=' etc
+
+let c=7; // '=' is assignment operator which assigned the '7' to the variable c
+console.log(c);
+c+=10
+console.log(c) // 17
+c-=10;
+console.log(c) // 7
+ 
+c*=2
+console.log(c) // 14
+
+//  Comparison Operators : '==' , '===' , '!=' , '!==' ,'>' , '>=' , '<' ,'<='
+console.log(5=="5") // true == check only value not type
+console.log(5==="5") // false === check value and type
+console.log(5>8) // false
+console.log(5<8) // true
+console.log(5!=8) // true 
+console.log(5!=="5") // true
+
+// Logical Operators : && , || , !
+
+console.log(6> 5 && 6<7) // true
+console.log(6> 5 || 6>7)  // true
+console.log(!(6>5 && 6<7)) // false
+
+// Bitwise Operators:  '&' , '|' , '~' 
+console.log(5&1) // 1
+console.log(5|1) //5
+console.log(~5)  // -6
+console.log(5^2)  // 7
+console.log(5<<1) //10
+console.log(5>>1) // 2
+
+
+
